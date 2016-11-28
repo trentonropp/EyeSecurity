c3002a0bf9ef0056d8f8e969b0699215e8fbf050
apardalos <apardalos@gmail.com>
2016-11-28 15:31:15 -0500

    Compare
    work on Compare

--- Compare.h
+++ Compare.h
@@ -3,14 +3,14 @@
 
 class Compare {
 private:
-	Pixel[] * iris1;
-	Pixel[] * iris2;
+	std::vector<Pixel>  iris1;
+	std::vector<Pixel>  iris2;
 	void equalize_Arrays();
 	double find_Brightness_Differences();
 	std::vector<int> find_Pattern_Differences();
 	double find_Percent_Similarity();
 public:
-	Compare(Pixel[] &iris1, Pixel[] &iris2);
+	Compare(std::vector<Pixel> iris1, std::vector<Pixel> iris2);
 	bool is_same_person();
 
 };
