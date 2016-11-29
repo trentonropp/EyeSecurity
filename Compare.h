fe01c59529c8aff74dbb848a5d6d332e82d2fa4e
apardalos <apardalos@gmail.com>
2016-11-28 21:29:22 -0500

    Compare almost done
    only need Jad to do the brightness difference

--- Compare.h
+++ Compare.h
@@ -5,12 +5,15 @@ class Compare {
 private:
 	std::vector<Pixel>  iris1;
 	std::vector<Pixel>  iris2;
+	std::vector<int>  color_differences;
+	double percent_color_difference;
+	bool is_same;
 	void equalize_Arrays();
-	double find_Brightness_Differences();
+	void correct_Brightness_Differences();
 	std::vector<int> find_Pattern_Differences();
-	double find_Percent_Similarity();
+	void find_Percent_Similarity();
 public:
 	Compare(std::vector<Pixel> iris1, std::vector<Pixel> iris2);
-	bool is_same_person();
+	void is_same_person();
 
 };
