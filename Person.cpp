2e53ecc99be7f9bd702bb382f3090cd1d6981343
Alec Unland <aunland@hotmail.com>
2016-12-02 18:03:03 -0500

    Update Person.cpp
    +some comments

--- Person.cpp
+++ Person.cpp
@@ -8,16 +8,19 @@
 
 using namespace std;
 
+//returns the name of the person
 string getName()
 {
     return name;
 }
 
+//adds a new iris to a person's vector of iris'
 void addIrisInstance(vector <Pixel> iris)
 {
     irisArrays.push_back(iris);
 }
 
+//calculates the average RGB value of the persons iris array
 vector<Pixel> getAverage()
 {
     
