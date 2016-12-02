15672bdc7555a9f81505ae020b42fae19467095e
CalamariDude <zeineddine.jad@gmail.com>
2016-12-02 17:57:10 -0500

    Add files via upload
    yo my name is jad but im not that bad catch me in the library checkin out yo dad

--- Parser.cpp
+++ Parser.cpp
@@ -3,20 +3,57 @@ using namespace std;
 
 
 Parser::Parser(vector <vector<Pixel> > &pic) {
+    picture=pic;
+    //resize bw
+    BWArray.resize(pic.size(),vector<Pixel>(pic[0].size(),Pixel()));
 
 }
 
+
+void Parser::make_Pupil_BW(){
+    for (int i = 0; i< picture.size(); i++) {
+        for (int j = 0; j < picture[i].size(); j++) {
+        
+            if(picture[i][j].isPrettyBlack(picture[i][j])){
+                BWArray[i][j].makeBlack(BWArray[i][j]);
+            }
+            else{
+                BWArray[i][j].makeWhite();
+            }
+        
+        
+        }
+        
+    }
+    
+}
+
+
+
+
 vector<int> Parser::find_Pupil() {
     vector<int> coordinates;
+    //weighted center vector
+    vector<vector<int> > a;
     
-    
-    
+    //needs to find a circle by some predefined contingincies depending on the width and heigh of image. Basically, image needs to be certain size and the eye must be close enough
     
     
     return coordinates;
 
 }
 
+vector<int> Parser::getWeightedCenter(vector<vector<int> >){
+    //error catch more than inner vector of 3 length
+    //inner vector should be <number of blacks, j>
+    
+     //follow algorithm on boogie board
+    vector<int> a;
+    
+    
+    
+    return a;
+}
 
 
 vector<Pixel> Parser::get_Iris_Array(int x, int y, int radius) {
--- Parser.h
+++ Parser.h
@@ -6,9 +6,13 @@ using namespace std;
 class Parser {
 
 private:
-
+    vector<vector <Pixel> > BWArray;
     vector<vector<Pixel> > picture;
+    void make_Pupil_BW();
     vector<int> find_Pupil();
+    vector<int> getWeightedCenter(vector<vector<int> >);
+
+    
 public:
 	Parser(vector <vector<Pixel> > &pic);
     vector <Pixel> get_Iris_Array(int x, int y, int radius);
