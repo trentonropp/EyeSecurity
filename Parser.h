f7a2d7b5c8a94f9676a098557c5e004db99bda85
trentonropp <trentonropp@modem-6.local>
2016-12-04 18:08:38 -0500

    idek what this is

--- Parser.h
+++ Parser.h
@@ -9,28 +9,28 @@ class Parser {
 private:
 
 
-    void makePupilBW();
-    vector<int> findPupil();
-    vector<int> getWeightedCenter(vector<vector<int> >, int bound);
-    int largestPupilRadius = 0;
-    int minRadius;
-    int varianceInRadius;
-    int getUpRadius(int i , int j );
-    int getDownRadius(int i , int j);
-    int getLeftRadius(int i, int j);
-    int getRightRadius(int i , int j);
-    bool isLargestRadius(vector<int> radii);
-    bool isCircularEnough(vector<int> radii);
-    bool isLargeEnough(vector<int> radii);
-    int getAvg(vector<int> radii);
-    int pupilI=0;
-    int pupilJ=0;
-    vector<int> getRadiusVector(int i, int j);
-    int getLeftEdgeCoordinates();
-
+  void makePupilBW();
+  vector<int> findPupil();
+  vector<int> getWeightedCenter(vector<vector<int> >, int bound);
+  int largestPupilRadius = 0;
+  int minRadius;
+  int varianceInRadius;
+  int getUpRadius(int i , int j );
+  int getDownRadius(int i , int j);
+  int getLeftRadius(int i, int j);
+  int getRightRadius(int i , int j);
+  bool isLargestRadius(vector<int> radii);
+  bool isCircularEnough(vector<int> radii);
+  bool isLargeEnough(vector<int> radii);
+  int getAvg(vector<int> radii);
+  int pupilI=0;
+  int pupilJ=0;
+  vector<int> getRadiusVector(int i, int j);
+  int getLeftEdgeCoordinates();
+  
 public:
-    vector<vector<Pixel> > picture;
-    vector<vector <Pixel> > BWArray;
-	Parser(vector <vector<Pixel> > &pic);
-    vector <Pixel> getIrisArray();
+  vector<vector<Pixel> > picture;
+  vector<vector <Pixel> > BWArray;
+  Parser(vector <vector<Pixel> > &pic);
+  vector <Pixel> getIrisArray();
 };
