89bf317632487411b77dc4fe2325e47f5a7fde43
Alec Unland <aunland@hotmail.com>
2016-12-04 17:39:22 -0500

    Update Person.cpp

--- Person.cpp
+++ Person.cpp
@@ -1,27 +1,47 @@
 //
 //  Person.cpp
-//  Created by Alec Unland on 12/1/16.
+//  Created by Alec Unland
 //
 
-#include "Person.hpp"
+#include "Person.h"
 #include <vector>
+#include <stdlib.h>
 
 using namespace std;
 
 //returns the name of the person
-string getName()
+string Person::getName()
 {
     return name;
 }
 
 //adds a new iris to a person's vector of iris'
-void addIrisInstance(vector <Pixel> iris)
+void Person::addIrisInstance(vector <Pixel> iris)
 {
     irisArrays.push_back(iris);
+    return;
 }
 
-//calculates the average RGB value of the persons iris array
-vector<Pixel> getAverage()
+//removes an iris from irisArrays
+void Person::removeIrisInstance(int index)
 {
+    irisArrays.erase(index);
+    return;
+}
+
+//returns the iris from the given index of irisArrays
+vector<Pixel> Person::getAverage(int index)
+{
+    vector<Pixel> avg;
     
+    return irisArrays[index];
+}
+
+//reurns a random iris from irisArrays
+vector<Pixel> Person::getRandom()
+{
+    //generates random number between 0 and the size of irisArrays
+    v1 = rand() % irisArrays.size();
+    
+    return irisArrays[v1];
 }
