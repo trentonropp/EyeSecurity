3f700c38460a105b94a085ef0409815d9125ed4a
Alec Unland <aunland@hotmail.com>
2016-12-04 17:40:01 -0500

    Update Person.h

--- Person.h
+++ Person.h
@@ -1,10 +1,10 @@
 //
 //  Person.h
-//  Created by Alec Unland on 12/1/16.
+//  Created by Alec Unland
 //
 
 #include <vector>
-#include "Pixel.hpp"
+#include "Pixel.h"
 
 using namespace std;
 
@@ -12,9 +12,11 @@ class Person{
     
 private:
     vector< vector<Pixel> > irisArrays;
+        //used to hold multiple vectors of pixels taken from the iris from different pictures
     string name;
     
 public:
+    //constructor
     Person(string name)
     {
         this->name = name;
@@ -22,6 +24,11 @@ public:
     
     string getName();
     void addIrisInstance(vector <Pixel>);
-    vector<Pixel> getAverage();
-    
+        //adds an iris vector to a person's collection of different iris' (irisArrays)
+    void removeIrisInstance(int index)
+        //removes an iris from irisArrays
+    vector<Pixel> getAverage(int index);
+        //returns the iris at the given index of irisArrays 
+    vector<Pixel> getRandom();
+        //returns a random iris from irisArrays
 };
